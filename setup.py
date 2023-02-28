b4b8e80e0f519ee5ea91bc85aedb87cc5308183c
Adam Schubert <adam.schubert@sg1-game.net>
2023-02-28 22:26:30 +0100

    New version 1.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='instagram-basic-display',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(exclude=['tests', 'tests.*']),
     package_data={'instagram_basic_display': ['py.typed']},
     install_requires=[
